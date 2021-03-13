3e5f386e270fdc618afffcd673ed56430b21795e
Debashis Ray <debaray14@gmail.com>
2021-03-14 01:26:44 +0600

    almost done

--- src/components/LeagueDetails/LeagueDetails.js
+++ src/components/LeagueDetails/LeagueDetails.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import './LeagueDetails.css';
+import image from './male.png';
 const LeagueDetails = () => {
     const {leagueId} = useParams();
     const [leagues,setLeague] = useState([]);
@@ -17,8 +18,18 @@ const LeagueDetails = () => {
             <div className="league-banner col-sm-12">
                 <img className="banner-design" src={leagues.map(league =>league.strBanner)} alt=""/>
             </div>
-            <div className="mt-5 container">
-                <div className="row text-center justify-content-center">
+            <div className="mt-5 container leagues-style">
+                <div className="league-details row d-flex ">
+                    <div className="league-info col-sm-12 col-md-6 col-lg-6">
+                        <h1>Premier League</h1>
+                        <h4>Founded: February 20, 1992</h4>
+                        <h4>Country: England</h4>
+                        <h4>Sport Type: Football</h4>
+                        <h4>Gender: Male</h4>
+                    </div>
+                    <div className="col-sm-12 col-md-6 col-lg-6 border">
+                        <img className="image-design"src={image} alt="male"/>
+                    </div>
                 </div>
 
             </div>
